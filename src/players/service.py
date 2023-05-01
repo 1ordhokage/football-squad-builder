84505976b3ee2a8011d6d9377c8650f29298325c
Jurabek Aminov <dzhurabek.aminov@gmail.com>
2023-05-01 22:24:27 +0300

    players init

--- src/players/service.py
+++ src/players/service.py
@@ -19,7 +19,7 @@ class PlayersService:
         if player is None:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail="Player not found."
+                detail='Player not found.'
             )
         return player
 
