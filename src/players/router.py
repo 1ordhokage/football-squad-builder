9e7aa8f40af0b0f0762a1ac488f2895e042ea73f
Jurabek Aminov <dzhurabek.aminov@gmail.com>
2023-05-02 05:28:49 +0300

    update

--- src/players/router.py
+++ src/players/router.py
@@ -11,10 +11,5 @@ router = APIRouter(
 
 
 @router.get('/{id}', response_model=PlayerRead)
-async def get_operations(id: int, players_service: PlayersService = Depends()):
+async def get_player(id: int, players_service: PlayersService = Depends()):
     return await players_service.get_by_id(id)
-
-
-@router.get('/name={name}', response_model=list[PlayerRead])
-async def get_operations(name: str, players_service: PlayersService = Depends()):
-    return await players_service.get_by_name(name)
